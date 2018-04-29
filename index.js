76a6113700369d3204112acd78759eb6ec57fa62
blakestone95 <blakestone.trumpet1@gmail.com>
2018-04-29 15:00:42 -0700

    Set up very basic React app with Webpack.
    Builds to /dist (which should not be committed, already removed in .gitignore).

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+import App from './src/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+// import favicon from '../assets/images/favicon.ico';
+
+export default class App extends Component {
+  render() {
+    return <p>This is my new react app</p>;
+  }
+}
