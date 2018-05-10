5dc850e587ead367451bf448f1fa3d793182330e
blakestone95 <blakestone.trumpet1@gmail.com>
2018-05-09 21:27:18 -0700

    Add styles file for navbar.

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -7,11 +7,21 @@ class NavBar extends Component {
   render() {
     return (
       <div className="legion-navbar">
-        <Link to={ROUTE.home} replace >Home</Link>
-        <Link to={ROUTE.tab2} replace >Tab2</Link>
-        <Link to={ROUTE.tab3} replace >Tab3</Link>
-        <Link to={ROUTE.tab4} replace >Tab4</Link>
-        <Link to={ROUTE.tab5} replace >Tab5</Link>
+        <Link to={ROUTE.home} replace>
+          Home
+        </Link>
+        <Link to={ROUTE.tab2} replace>
+          Tab2
+        </Link>
+        <Link to={ROUTE.tab3} replace>
+          Tab3
+        </Link>
+        <Link to={ROUTE.tab4} replace>
+          Tab4
+        </Link>
+        <Link to={ROUTE.tab5} replace>
+          Tab5
+        </Link>
       </div>
     );
   }
