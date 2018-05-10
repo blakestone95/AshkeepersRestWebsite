bfddcd87bdcd5ea69a52043b0df33b3db84e8b50
blakestone95 <blakestone.trumpet1@gmail.com>
2018-05-09 20:51:22 -0700

    Implement basic routing and navigation bar.  Add history package.  Add enumerations.

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
-export default class Home extends Component {
+class Home extends Component {
   render() {
     return <div>This is the Home page</div>;
   }
 }
+
+export default Home;
