27854d31c5e1cdfa4c8df3526215868ca5a3ef42
blakestone95 <blakestone.trumpet1@gmail.com>
2018-06-01 23:06:27 -0700

    Add built files to appropriate Laravel folders for backend configuration testing.  Remove more unneeded files.

---
+++ Legion.BackEnd/resources/views/index.blade.php
@@ -0,0 +1,12 @@
+2<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>73rd Legion</title>
+<link rel="shortcut icon" href="/favicon.ico"></head>
+
+<body>
+  <div id="app"></div>
+<script type="text/javascript" src="/js/main.bundle.js"></script></body>
+
+</html>
