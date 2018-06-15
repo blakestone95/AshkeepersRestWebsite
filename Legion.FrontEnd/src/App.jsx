62e2459a8a4d91cf2fff265d65f743901b421b23
blakestone95 <blakestone.trumpet1@gmail.com>
2018-06-14 19:58:54 -0700

    Improve configuration. Rename some stuff.  Run Prettier with new configurations over files.

---
+++ Legion.FrontEnd/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+// Components
+import Home from 'Home';
+import NavBar from 'NavBar';
+
+import { ROUTE } from 'util/enums';
+
+import 'style.less';
+
+export const history = createHistory();
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="legion-body">
+          <NavBar />
+          <div className="legion-page-container">
+            <div className="legion-sidebar-left" />
+            <Switch>
+              <Redirect exact from="/" to={ROUTE.home} />
+
+              <Route exact path={ROUTE.home} component={Home} />
+              <Route exact path={ROUTE.tab2} component={Home} />
+              <Route exact path={ROUTE.tab3} component={Home} />
+              <Route exact path={ROUTE.tab4} component={Home} />
+              <Route exact path={ROUTE.tab5} component={Home} />
+            </Switch>
+            <div className="legion-sidebar-right" />
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
