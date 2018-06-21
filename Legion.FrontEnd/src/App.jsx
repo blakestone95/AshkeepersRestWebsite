d84df0a99dbf7918bc6599d9458eb50b1bf57ce8
blakestone95 <blakestone.trumpet1@gmail.com>
2018-06-20 19:59:44 -0700

    Move some of the layout stuff around.
    #1

--- Legion.FrontEnd/src/App.jsx
+++ Legion.FrontEnd/src/App.jsx
@@ -19,35 +19,37 @@ export default class App extends Component {
           <NavBar />
           <div className="legion-page-container">
             <div className="legion-sidebar-left" />
-            <Switch>
-              <Redirect exact from="/" to={PATHS.home} />
+            <div className="legion-page-content">
+              <Switch>
+                <Redirect exact from="/" to={PATHS.Home.path} />
 
-              <Route
-                exact
-                path={PATHS.Home.path}
-                component={PATHS.Home.component}
-              />
-              <Route
-                exact
-                path={PATHS.Tab2.path}
-                component={PATHS.Tab2.component}
-              />
-              <Route
-                exact
-                path={PATHS.Tab3.path}
-                component={PATHS.Tab3.component}
-              />
-              <Route
-                exact
-                path={PATHS.Tab4.path}
-                component={PATHS.Tab4.component}
-              />
-              <Route
-                exact
-                path={PATHS.Tab5.path}
-                component={PATHS.Tab5.component}
-              />
-            </Switch>
+                <Route
+                  exact
+                  path={PATHS.Home.path}
+                  component={PATHS.Home.component}
+                />
+                <Route
+                  exact
+                  path={PATHS.Tab2.path}
+                  component={PATHS.Tab2.component}
+                />
+                <Route
+                  exact
+                  path={PATHS.Tab3.path}
+                  component={PATHS.Tab3.component}
+                />
+                <Route
+                  exact
+                  path={PATHS.Tab4.path}
+                  component={PATHS.Tab4.component}
+                />
+                <Route
+                  exact
+                  path={PATHS.Tab5.path}
+                  component={PATHS.Tab5.component}
+                />
+              </Switch>
+            </div>
             <div className="legion-sidebar-right" />
           </div>
         </div>
