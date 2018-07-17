f9ff90c65a9b069493154a6735dcfb9bf63745b4
dakriy <git@persignum.com>
2018-07-17 12:36:38 -0700

    Update Announcement.php
    Add the deleted_at field back in just in case deleted entries are requested and wanted, in which case this field would be wanted.

--- Legion.BackEnd/app/Announcement.php
+++ Legion.BackEnd/app/Announcement.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Announcement extends Model
 {
     /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = ['deleted_at'];
-
-    /**
      * The attributes that are mass assignable.
      *
      * @var array
