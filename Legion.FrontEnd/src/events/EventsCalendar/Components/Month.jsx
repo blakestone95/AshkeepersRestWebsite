56f0430d45782c138b1e8f0a5289b8b37f43dbbf
chiq2045 <charles.oroko@outlook.com>
2018-08-03 03:04:00 -0700

    Renders one month (a collection of 4-6 weeks).
    #12

--- Legion.FrontEnd/src/events/EventsCalendar/Components/Month.jsx
+++ Legion.FrontEnd/src/events/EventsCalendar/Components/Month.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import Week from './Week';
+import moment from 'moment';
 
 class Month extends React.PureComponent {
+  state = {
+    month: this.props.month
+  }
+
+  createWeeks = (month) => {
+    var weeks = [],
+        date = month.clone().startOf('month').startOf('week'),
+        nextMonth = date.clone().add(1, 'M'),
+        done = false;
+    while (!done) {
+      weeks.push(<span><Week date={date} /></span>)
+      date = date.clone().add(1, 'w')
+      done = date.month() !== nextMonth.month()
+    }
+    
+    return weeks
+  }
+  
   render() {
     return(
       <div>
-        Render a single Month
-        <Week />
+        {this.createWeeks(this.state.month)}
       </div>
     )
   }
