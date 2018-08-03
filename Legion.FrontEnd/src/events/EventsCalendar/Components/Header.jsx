0d90f3583febdafc8fec769897cb9b462d81afdc
chiq2045 <charles.oroko@outlook.com>
2018-08-03 03:51:59 -0700

    Adds a navigational header to the calendar.
    Header contains the current month and year, a previous and a next button.
    
    #12

--- Legion.FrontEnd/src/events/EventsCalendar/Components/Header.jsx
+++ Legion.FrontEnd/src/events/EventsCalendar/Components/Header.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import moment from 'moment';
 
 class Header extends React.PureComponent {
+  state = {
+    month: this.props.current,
+    date: this.props.selected
+  }
+  
+  nextMonth = (month) => {
+    this.setState({ month:this.state.month.add(1, 'M') })
+  }
+
+  previousMonth = () => {
+    this.setState({ month:this.state.month.subtract(1, 'M') })
+  }
+
+  onDateClick = (selected) => {
+    this.setState({ date:selected })
+  }
+  
   render() {
-    return <div>Header</div>
+    return(
+      <div>
+        <div onClick={this.previousMonth()}>Previous</div>
+        <span>{this.state.month.format('MMMM YYYY')}</span>
+        <div onClick={this.nextMonth()}>Next</div>
+      </div>
+    )
   }
 }
 
