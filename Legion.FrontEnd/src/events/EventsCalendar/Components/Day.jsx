dfc7841c8a89e140e3ee5f957714d173a721e85f
chiq2045 <charles.oroko@outlook.com>
2018-08-03 03:01:55 -0700

    Renders one day.
    #12

--- Legion.FrontEnd/src/events/EventsCalendar/Components/Day.jsx
+++ Legion.FrontEnd/src/events/EventsCalendar/Components/Day.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import moment from 'moment';
 
 class Day extends React.PureComponent {
+  state = {
+    date: this.props.date
+  }
   render() {
+    var day = {
+        name:    this.state.date.format('ddd'),
+        number:  this.state.date.date(),
+        date:    this.state.date
+    }
+    var tomorrow =  this.state.date.clone().add(1, 'd');
+    var yesterday = this.state.date.clone().subtract(1, 'd');
     return(
-      <div>Render a single Day</div>
+      <div>
+        {day.date.format('ddd Do MMM YY')}
+      </div>
     )
   }
 }
