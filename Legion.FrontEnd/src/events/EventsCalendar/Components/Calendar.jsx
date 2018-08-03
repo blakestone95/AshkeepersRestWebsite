bc5d001ec7c5a7fb812dfdca54b8361cfa21a7e7
chiq2045 <charles.oroko@outlook.com>
2018-08-03 03:52:50 -0700

    Renders the calendar as a whole.
    #12

--- Legion.FrontEnd/src/events/EventsCalendar/Components/Calendar.jsx
+++ Legion.FrontEnd/src/events/EventsCalendar/Components/Calendar.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import Header from './Header';
 import DaysOfWeek from './DaysOfWeek';
 import Month from './Month';
+import moment from 'moment';
 
 class Calendar extends React.PureComponent {
+  state = {
+    currentMonth: new moment,
+    selectedDate: new moment
+  }
+
   render() {
     return( 
       <div>
-        <Header />
+        <Header current={this.state.currentMonth} selected={this.state.selectedDate} />
         <DaysOfWeek />
-        <Month />
+        <Month month={this.state.currentMonth}/>
       </div>
     )
   }
