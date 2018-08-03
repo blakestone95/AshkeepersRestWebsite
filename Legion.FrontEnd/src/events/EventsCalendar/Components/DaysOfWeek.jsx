ed5617a94ef1b2117f8760a188e756617cbdcf95
chiq2045 <charles.oroko@outlook.com>
2018-08-03 03:07:44 -0700

    Renders the days of the week as column headers.
    #12

--- Legion.FrontEnd/src/events/EventsCalendar/Components/DaysOfWeek.jsx
+++ Legion.FrontEnd/src/events/EventsCalendar/Components/DaysOfWeek.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import moment from 'moment';
 
 class DaysOfWeek extends React.PureComponent {
+  listDays = () => {
+    var names = []
+    for (var i=0; i<7; i++) {
+      names.push(<span>{moment().day(i).format('ddd')}</span>)
+    }
+    return names
+  }
   render() {
     return(
-      <div>The days of the week in a row</div>
+      <div>{this.listDays()}</div>
     )
   }
 }
