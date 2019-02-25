3fc51087205e88a16f84a7dcd594c5dd8ffefb5b
blakestone95 <blakestone.trumpet1@gmail.com>
2019-02-24 21:14:21 -0800

    Add an enumeration of HTTP methods
    #34

--- Ash.FrontEnd/src/js/global/components/Fido.jsx
+++ Ash.FrontEnd/src/js/global/components/Fido.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import memoizeOne from 'memoize-one';
 
 /**
+ * Enumeration of all HTTP request methods fetch can use
+ * Contains all HTTP method listed on the following MDN page as of (2/24/19):
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+ */
+export const HTTP_METHODS = {
+  get: 'GET',
+  head: 'HEAD',
+  post: 'POST',
+  put: 'PUT',
+  delete: 'DELETE',
+  connect: 'CONNECT',
+  options: 'OPTIONS',
+  trace: 'TRACE',
+  patch: 'PATCH',
+};
+
+/**
  * Fetch configuration object default object shape
  * @prop {string} path - fetch url
  * @prop {object} query - object containing query string key-value pairs
@@ -18,7 +35,7 @@ const defaultFetchConfig = {
   query: null,
   payload: null,
   options: {
-    method: 'get',
+    method: HTTP_METHODS.get,
     fetchImmediately: true,
     preservePreviousData: true,
   },
@@ -173,7 +190,7 @@ class Fido extends React.Component {
     const abortController = new AbortController();
 
     let body = null;
-    if (method !== 'GET' && method !== 'HEAD') {
+    if (![HTTP_METHODS.get, HTTP_METHODS.head].includes(method)) {
       body = JSON.stringify(config.payload);
     }
 
