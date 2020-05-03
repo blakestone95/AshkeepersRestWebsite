5010997ff5d5ff11b6db17419ad0d604ca8cc6c4
blakestone95 <blakestone.trumpet1@gmail.com>
2020-05-02 22:22:33 -0700

    Flesh out announcements listing page

--- client/src/js/announcements/AnnouncementBoard.jsx
+++ client/src/js/announcements/AnnouncementBoard.jsx
@@ -1,9 +1,34 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { useFido, Loading, SectionTile } from 'components';
 
-class AnnouncementBoard extends PureComponent {
-  render() {
-    return <div>This is the Announcement Board page</div>;
-  }
+const config = { path: '/api/announcements' };
+
+function AnnouncementBoard(/* props */) {
+  const [announcements] = useFido(config);
+
+  return (
+    <>
+      <h1>Announcements</h1>
+      <Loading isLoading={announcements.inFlight}>
+        {/* Known bug in eslint-plugin-react 
+            https://github.com/yannickcr/eslint-plugin-react/issues/2571
+            2020-5-2 */}
+        {/* eslint-disable-next-line react/display-name */}
+        {announcements.data?.Announcements.map((announcement) => {
+          const { id, title, created_at, user_id, content } = announcement;
+          return (
+            <SectionTile
+              key={id}
+              heading={title}
+              date={created_at}
+              author={user_id}
+              text={content}
+            />
+          );
+        }) || null}
+      </Loading>
+    </>
+  );
 }
 
 export default AnnouncementBoard;
