fcd7ac86bc011a051c620b3c6e637da737d4f95d
Blake <blakestone.trumpet1@gmail.com>
2021-04-07 21:59:03 -0700

    Restyle nav bar to fit new design

--- client/src/js/App.jsx
+++ client/src/js/App.jsx
@@ -11,7 +11,6 @@ export default function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <hr className="ash-divider" />
       <main className="ash-main">
         <Routing />
       </main>
